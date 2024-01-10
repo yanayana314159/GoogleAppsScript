e1b059ba6db64eb4c3b426df4cedabcb0bb4982e
Yana <64722020+yanayana314159@users.noreply.github.com>
2024-01-11 00:09:10 +0900

    Update and rename カレンダー連携.js to main.js

--- main.js
+++ main.js
@@ -1,22 +1,3 @@
-//ディスコードに送る関数
-function senddisco(sentcon) {
-    const WEBHOOK_URL = "";//取得したWebhookURLを追加
-
-    const payload = {
-        username: "XXXX", //好きな名前を入れてください
-        content: sentcon,
-    };
-
-    UrlFetchApp.fetch(WEBHOOK_URL, {
-        method: "post",
-        contentType: "application/json",
-        payload: JSON.stringify(payload),
-    });
-
-}
-
-
-
 //カレンダーから情報を抜き取る関数
 function getcalendar(events, date) {
     const strarray = [];
